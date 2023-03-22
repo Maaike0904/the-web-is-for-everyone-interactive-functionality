63f8510d2346fc7ef903bddd309a732d7f3a33c2
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-03-22 14:10:33 +0100

    Update

--- index.js
+++ index.js
@@ -26,13 +26,74 @@ server.get("/", (request, response) => {
   });
 });
 
-// dit plak je aan de basis url van de api, /producten
+// dit plak je aan de basis url van de api, /producten (Voor in de kalender)
 server.get("/", async (request, response) => {
   let productenUrl = url + "/producten";
 
   fetchJson(productenUrl).then((data) => {
     response.render("index", data);
   });
+});
+
+server.get("/", (request, response) => {
+  response.render("index");
+});
+
+server.post("/", (request, response) => {
+  const url = "https://whois.fdnd.nl/api/v1";
+
+  postJson(url, request.body).then((data) => {
+    // ... zorgt ervoor dat de invoelvelden ingevuld blijven, zodat je het niet steeds opnieuw in hoeft te vullen bij een error
+    let newComment = { ...request.body };
+
+    if (data.success) {
+      response.redirect("/?commentPosted=true");
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
+    } else {
+      const errormessage = `${data.message}: Heb je alles goed ingevuld?.`;
+      const newdata = { error: errormessage, values: newComment };
+
+      response.render("index", newdata);
+    }
+  });
+});
+
+// pagina's zonder inhoud van andere allergenen
+server.get("/Ei", (request, response) => {
+  response.render("Ei");
+});
+
+server.get("/Pinda", (request, response) => {
+  response.render("Pinda");
+});
+
+server.get("/Amandel", (request, response) => {
+  response.render("Amandel");
+});
+
+server.get("/Schelp", (request, response) => {
+  response.render("Schelp");
+});
+
+server.get("/Soja", (request, response) => {
+  response.render("Soja");
+});
+
+server.get("/Vis", (request, response) => {
+  response.render("Vis");
+});
+
+server.get("/Hazelnoot", (request, response) => {
+  response.render("Hazelnoot");
+});
+
+server.get("/Walnoot", (request, response) => {
+  response.render("Walnoot");
+});
+
+server.get("/Cashewnoot", (request, response) => {
+  response.render("Cashewnoot");
 });
 
 // Stel het poortnummer in en start express
