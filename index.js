b44213b2b6405c66a00c476397276e0568612218
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-03-29 14:05:10 +0200

    Post werkt :)

--- index.js
+++ index.js
@@ -1,72 +1,91 @@
+//import express en dotenv
 import express from "express";
 
-// Link naar de api van vini mini
-const url = "https://api.vinimini.fdnd.nl/api/v1";
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error);
-
-// Maak een nieuwe express server
+//maak een nieuwe express app
 const server = express();
 
-// Stel in hoe we express gebruiken
-server.set("view engine", "ejs");
-server.set("views", "./views");
+//public map gebruiken
 server.use(express.static("public"));
 
-// Maak een route naar de index.ejs voor categorieën
-server.get("/", (request, response) => {
-  let categoriesUrl = url + "/categories";
+//stel de views in
+server.set("view engine", "ejs");
+server.set("views", "./views");
 
-  fetchJson(categoriesUrl).then((data) => {
-    response.render("index", data);
-  });
-});
-
-// Maak een route naar de ei.ejs
-server.get("/ei", async (request, response) => {
-  let productenUrl = url + "/producten";
-
-  await fetchJson(productenUrl).then((data) => {
-    response.render("ei", data);
-  });
-});
-
-// Maak een route naar de pinda.ejs
-server.get("/pinda", async (request, response) => {
-  let productenUrl = url + "/producten";
-
-  await fetchJson(productenUrl).then((data) => {
-    response.render("pinda", data);
-  });
-});
-
-// FORMULIER NOTITIE
-// Stel de afhandeling van formulieren in
+// Stel afhandeling van formulieren in
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-// Maak een route voor de index
+//hier komen de routes
+
 server.get("/", (request, response) => {
   const baseUrl = "https://api.vinimini.fdnd.nl/api/v1/";
-  const pepijnId = "notities?id=clemozv3c3eod0bunahh71sx7";
-  const url = `${baseUrl}${pepijnId}`;
+  const samId = "notities?id=clemozv3c3eod0bunahh71sx7";
+  const url = `${baseUrl}${samId}`;
 
   fetchJson(url).then((data) => {
     response.render("index", data);
   });
 });
 
-// Stel het poortnummer in en start express
-server.set("port", process.env.PORT || 4001);
-server.listen(server.get("port"), function () {
-  console.log(
-    `serverlication started on http://localhost:${server.get("port")}`
-  );
+// Dit is de data uit whois api // Met deze stuk code willen we de data roepen uit de whois api?
+server.post("/", function (req, res, next) {
+  const baseurl = "https://api.vinimini.fdnd.nl/api/v1/";
+  const url = `${baseurl}`;
+  req.body.afgerond = false;
+  req.body.persoonId = "clemozv3c3eod0bunahh71sx7";
+  req.body.datum = req.body.datum + ":00Z";
+  req.body.herinnering = [req.body.herinnering + ":00Z"];
+  console.log(req.body);
+
+  //
+  postJson(url + "/notities", req.body).then((data) => {
+    console.log(JSON.stringify(data));
+    let newNotitie = { ...req.body };
+
+    if (data.success) {
+      res.redirect("/");
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
+    } else {
+      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`;
+      const newdata = { error: errormessage, values: newData };
+
+      res.render("index", newdata);
+    }
+  });
 });
 
+// Stel het poortnummer in en start express
+server.set("port", process.env.PORT || 8000);
+server.listen(server.get("port"), function () {
+  console.log(`Application started on http://localhost:${server.get("port")}`);
+});
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
 async function fetchJson(url) {
   return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
+
+/**
+ * postJson() is a wrapper for the experimental node fetch api. It fetches the url
+ * passed as a parameter using the POST method and the value from the body paramater
+ * as a payload. It returns the response body parsed through json.
+ * @param {*} url the api endpoint to address
+ * @param {*} body the payload to send along
+ * @returns the json response from the api endpoint
+ */
+export async function postJson(url, body) {
+  return await fetch(url, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  })
     .then((response) => response.json())
     .catch((error) => error);
 }
